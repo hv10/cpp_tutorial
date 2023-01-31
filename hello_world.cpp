c5f08ce505acf37e99fa231358dad413bc905da0
hv10 <noeldanz@gmail.com>
2023-01-31 08:42:52 +0100

    introduce first simple task

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-using namespace std;
+using namespace std; // this can be omitted by using std::cout and std::endl
 
 int main()
 {
     cout << "Hello World!" << endl;
+    /*
+    try extending the program to print your name
+    use std::cout and std::endl, or use the using namespace std;
+    if you use the using namespace std; you can omit the std:: in front of cout and endl
+    */
     return 0;
 }
